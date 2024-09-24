0d323a819bc9c9d4d208d458843f3fbc71458b4b
Enis B. Tuysuz <enisbtuysuz@gmail.com>
2024-09-25 00:34:55 +0300

    Simplified block creation

--- dids/did.py
+++ dids/did.py
@@ -2,4 +2,7 @@ from did_resolver import Resolver, DIDResolutionResult
 from jwt.jwe import create_jwe
 from jwt.jwt import verify_jws
 from jwt.types import JWE
+from cacao.src.cacao import Cacao, CacaoBlock
+from cacao.src.verification import EIP191Verifier
+from cid import cid
 from utils import *
